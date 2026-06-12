5b6893e18ad6f5e8d7165613a9f73d886906f3e2
vinayak-nagelli <vinunagelli44@gmail.com>
2026-06-12 18:54:07 +0530

    1089. Duplicate Zeros

---
+++ Arrays/Duplicate_Zeros.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n=arr.length;
+
+        for(int i=0;i<n;i++){
+
+            if(arr[i]==0){
+                 for(int j=n-1;j>i;j--){
+                    arr[j]=arr[j-1];
+                 }
+
+                if(i+1<n){
+                    arr[i+1]=0;
+                    i++;
+                }
+            }
+        }
+    }
+}
